cce92ebb8c5557cc2428afc4d026a0fbde385a74
ranjurave <ranjurave@gmail.com>
2022-08-03 10:24:13 +1200

    readyu for designer review

--- src/navbar.js
+++ src/navbar.js
@@ -5,10 +5,10 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faBars } from '@fortawesome/free-solid-svg-icons';
 
 function Navbar() {
-    const [menuType, setMenu] = useState("menu-items-collapsed")
+    const [menuType, setMenu] = useState("menu-collapsed")
     const menuToggle = () => {
         console.log(menuType);
-        menuType === "menu-items-collapsed" ? setMenu("menu-items") : setMenu("menu-items-collapsed")
+        menuType === "menu-collapsed" ? setMenu("menu-expand") : setMenu("menu-collapsed")
     }
   return (
     <div className='navbar-menu'>
@@ -17,11 +17,25 @@ function Navbar() {
                 <img src = '/images/electric_car.jpg' alt='logo'/>
                 <h1>ECar</h1>
             </div>
-            <div onClick={menuToggle} className='menu-collapse-button' >
-                <FontAwesomeIcon icon={faBars} />
+                <div onClick={menuToggle} className='menu-mobile-button' >
+                        <FontAwesomeIcon icon={faBars} />
+                <div className={menuType}>
+                    <div className='menu-buttons'>
+                        <p>About</p>
+                    </div>
+                    <div className='menu-buttons'>
+                        <p>Inventory</p>
+                    </div>
+                    <div className='menu-buttons'>
+                        <p>Finance</p>
+                    </div>
+                    <button>
+                        Login
+                    </button>
+                </div>
             </div>
         </div>
-        <div className={menuType}>
+        <div className='menu-items'>
             <div className='menu-buttons'>
                 <p>About</p>
             </div>
