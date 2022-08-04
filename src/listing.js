ff7c00af3a86347d3258d27cc2ccb1f49004303a
ranjurave <ranjurave@gmail.com>
2022-08-04 22:41:44 +1200

    comments from designer addressed

--- src/listing.js
+++ src/listing.js
@@ -3,9 +3,13 @@ import './listing.css'
 function Listing() {
   return (
     <div className='listing'>
+      <div className='listing-image'>
         <img src = '/images/car1.jpg' alt='car'></img>
+      </div>
+      <div className='listing-details'>
         <h2>Kona EV</h2>
         <h5>2021 - Range 300km</h5>
+      </div>
     </div>
   )
 }
